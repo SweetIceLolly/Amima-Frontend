862c12b972bc423dcee3fa80cc7a76d11df5e930
Hanson Liang <handsomehanson233@gmail.com>
2022-05-10 01:56:50 -0400

    Add models and controllers

---
+++ src/app/controllers/user.controller.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class UserController {
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getUserInfo() {
+
+  }
+
+  googleLogin() {
+
+  }
+
+  facebookLogin() {
+
+  }
+
+  storeToken() {
+
+  }
+
+  updateUserInfo() {
+
+  }
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://amimaa.com/api',
+  imageUrl: 'https://amimaa.com/static/images',
 };

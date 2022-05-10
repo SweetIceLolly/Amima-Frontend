d68d1b65c57213e9a0f805e9f193529ba57ce47c
benlee327 <benlee327@gmail.com>
2022-05-10 11:31:04 -0400

    added image uploader incomplete

--- src/app/modules/elements/image_uploader/ImageUploader.component.ts
+++ src/app/modules/elements/image_uploader/ImageUploader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+var imageArray:any[];        
 
 @Component({
     selector: 'ImageUploader',
@@ -9,7 +10,23 @@ export class ImageUploaderComponent {
     constructor() {
 
     }
-
+    gallery: any[] = [];
+    count= 0;
+    loadFile(event: any) {
+        let image: any = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0]);
+        this.gallery[this.count]=image;
+        this.count = this.count + 1;
+      }
+    
+    gallary: string[] = [];
+    imageUpload(event: any) {
+      if (event.target.files && event.target.files[0]) {
+        const reader = new FileReader();
+        reader.readAsDataURL(event.target.files[0]);
+        reader.onload = (e: any) => { this.gallary.push(e.target.result);};
+      }  
+    }
     ngOnInit() {
 
     }

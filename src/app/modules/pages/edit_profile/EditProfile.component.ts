e6b4200ed66a12bfb2d5f27ebecb398e81c11652
proyetei <75600096+proyetei@users.noreply.github.com>
2022-05-12 12:10:20 -0400

    edit profile page

---
+++ src/app/modules/pages/edit_profile/EditProfile.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'EditProfile',
+  templateUrl: './EditProfile.component.html',
+  styleUrls: ['./EditProfile.component.css']
+})
+export class EditProfileComponent {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+
+  }
+}

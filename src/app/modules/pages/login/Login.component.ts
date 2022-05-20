e5c2c2d3cbe501028adb525f9d8195b0b69974ec
Hanson Liang <handsomehanson233@gmail.com>
2022-05-20 01:50:42 -0400

    Redo login page; Add google & facebook buttons

--- src/app/modules/pages/login/Login.component.ts
+++ src/app/modules/pages/login/Login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserController } from '../../../controllers/user.controller';
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: 'Login',
@@ -6,15 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./Login.component.css']
 })
 export class LoginComponent {
-  constructor() {
+  constructor(
+    private userCtrl: UserController
+  ) { }
 
+  ngAfterViewInit() {
+    // Initialize the Google login
+    (window as any).google.accounts.id.initialize({
+      client_id: environment.googleClientId,
+      callback: this.handleGoogleLogin
+    });
+
+    // Initialize the Facebook login
+    const fbJsUrl = `https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v13.0&appId=${environment.facebookAppId}&autoLogAppEvents=1`;
+    document.body.append(`
+      <div id="fb-root"></div>
+      <script async defer crossorigin="anonymous" src="${fbJsUrl}" nonce="lI4daaDa"></script>
+    `);
+
+    // Render the Google login button
+    (window as any).google.accounts.id.renderButton(
+      document.getElementById("googleBtn"),
+      { theme: "filled_blue", size: "large" }
+    );
   }
 
-  ngOnInit() {
-
+  handleGoogleLogin(response: any) {
+    console.log("Encoded JWT ID token: " + response.credential);
   }
 
-  ngOnDestroy() {
-
+  facebookLogin() {
+    this.userCtrl.facebookLogin();
   }
 }

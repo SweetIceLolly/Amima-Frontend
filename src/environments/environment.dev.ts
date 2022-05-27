a1445d73b5615f7742e2de453651534a0152fce3
Hanson Liang <handsomehanson233@gmail.com>
2022-05-27 17:44:16 -0400

    Change image urls

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  imageUrl: 'https://amimaa.com/static/images',
+  postImageUrl: 'http://localhost:8080/post_images',
+  profileImageUrl: 'http://localhost:8080/profile_images',
   googleClientId: '256920528847-au1fkfqhjf625id05pbflhdr0gmjlupr.apps.googleusercontent.com',
   facebookAppId: '976392646392425',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://amimaa.com/api',
-  imageUrl: 'https://amimaa.com/static/images',
+  postImageUrl: 'https://amimaa.com/post_images',
+  profileImageUrl: 'https://amimaa.com/profile_images',
   googleClientId: '256920528847-hd88t441v4d5798rshb5kf0ac4ar0c8d.apps.googleusercontent.com',
   facebookAppId: '379891797402168',
 };

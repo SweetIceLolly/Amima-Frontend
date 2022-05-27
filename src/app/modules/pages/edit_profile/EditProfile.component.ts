b36ca27d8f9df9ec72dd53d047e5776cb0173b33
John the second <wu103@mcmaster.ca>
2022-05-27 16:42:06 -0400

    edit profile with API

--- src/app/modules/pages/edit_profile/EditProfile.component.ts
+++ src/app/modules/pages/edit_profile/EditProfile.component.ts
@@ -2,21 +2,40 @@ import { Component } from '@angular/core';
 
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { UserController } from "../../../controllers/user.controller";
+import { User } from "src/app/models/User";
+
 @Component({
   selector: 'EditProfile',
   templateUrl: './EditProfile.component.html',
   styleUrls: ['./EditProfile.component.css']
 })
 export class EditProfileComponent {
-
+  currentUser: User = new User();
+  newName: string = "";
+  newEmail: string = "";
+  newBio: string = "";
   faQuestionCircle = faQuestionCircle;
   faCamera = faCamera;
-  constructor() {
 
+  constructor(
+    private userCtrl: UserController
+  ) {}
+
+  editProfile() {
+    let currentUserID = this.userCtrl.getLoggedInUser();
+    //let currentUser: User = this.userCtrl.getUserInfo(currentUserID);
+    let currentUser = new User();
+    currentUser.user_name = this.newName;
+    currentUser.email = this.newEmail;
+    currentUser.bio = this.newBio;
+    //currentUser.profile_image = TODO;
+    
+    this.userCtrl.editProfile(currentUser);
   }
 
-  ngOnInit() {
 
+  ngOnInit() {
   }
 
   ngOnDestroy() {

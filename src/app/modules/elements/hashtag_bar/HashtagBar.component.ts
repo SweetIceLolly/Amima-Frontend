704919fba30c19b6e64ec6a5b29b208c0c82ee10
Hanson Liang <handsomehanson233@gmail.com>
2022-05-31 18:23:50 -0400

    Fix create post UI

--- src/app/modules/elements/hashtag_bar/HashtagBar.component.ts
+++ src/app/modules/elements/hashtag_bar/HashtagBar.component.ts
@@ -2,16 +2,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'HashtagBar',
-    template: `<tag-input [(ngModel)]='items'></tag-input>`,
     templateUrl: './HashtagBar.component.html',
     styleUrls: ['./HashtagBar.component.css']
 })
 export class HashtagBarComponent {
 
     @Output() changeEvent = new EventEmitter<any[]>();
-    
+
     items = [];
-    
+
     constructor() {
 
     }

dcedf977c888c134c36ab21a96dee0d924a31891
Hanson Liang <handsomehanson233@gmail.com>
2022-05-31 11:27:01 -0400

    Update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://amimaa.com/api',
-  postImageUrl: 'https://amimaa.com/post_images',
-  profileImageUrl: 'https://amimaa.com/profile_images',
+  apiUrl: 'https://amimalive.com/api',
+  postImageUrl: 'https://amimalive.com/post_images',
+  profileImageUrl: 'https://amimalive.com/profile_images',
   googleClientId: '256920528847-hd88t441v4d5798rshb5kf0ac4ar0c8d.apps.googleusercontent.com',
   facebookAppId: '379891797402168',
 };

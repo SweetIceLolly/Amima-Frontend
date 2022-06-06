4d4196806b6635cc0e71f9dcef8d46f174d3b3eb
Hanson Liang <handsomehanson233@gmail.com>
2022-06-05 21:11:40 -0400

    Footer completely fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  postImageUrl: 'http://localhost:8080/post_images',
-  profileImageUrl: 'http://localhost:8080/profile_images',
+  apiUrl: 'https://amimalive.com/api',
+  postImageUrl: 'https://amimalive.com/api/post_images',
+  profileImageUrl: 'https://amimalive.com/api/profile_images',
   googleClientId: '256920528847-au1fkfqhjf625id05pbflhdr0gmjlupr.apps.googleusercontent.com',
   facebookAppId: '976392646392425',
 };

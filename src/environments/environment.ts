db561d14d3d74f1335a73cb01373f1bf16fe4443
Kartik Sharma <kartik.sharma345@gmail.com>
2022-08-05 06:39:41 +0300

    Fixed the category filter buttons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://dev.amimalive.com/api',
+  apiUrl: 'http://localhost:8080',
   postImageUrl: 'https://dev.amimalive.com/post_images',
   profileImageUrl: 'https://dev.amimalive.com/profile_images',
   googleClientId: '256920528847-au1fkfqhjf625id05pbflhdr0gmjlupr.apps.googleusercontent.com',
